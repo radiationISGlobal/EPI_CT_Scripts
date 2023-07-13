d01dc7ec6abbf6e04baad168702087e47db09424
radiationISGlobal <138576333+radiationISGlobal@users.noreply.github.com>
2023-07-13 12:51:40 +0200

    Add files via upload

---
+++ analysis_15/scripts/gather_results.r
@@ -0,0 +1,27 @@
+# Author:        Francesc Badia
+# Date:         2018-01-18 12:39:25
+# ------------------------------
+# Description:  
+# ------------------------------
+
+rm(list=ls())
+setwd('Y:/EPI CT Analysis/2022_new_grouping/')
+
+#outcomes_order <- c("leuk_noCLL","aleuk","AcLL","AcML","lymph","lymph_no_leuk_noCLL","Bcell_lymph","HD","NHL","Tcell_and_NK_cell")
+outcomes_order <- c("all_excl_therap_syndrel", "Lymphoid", "Myeloid")
+
+files <- list.files("analysis_15/results/",full.names = T)
+y <- data.frame()
+
+for(i in 1:length(files))
+{
+  x <- read.table(files[i],header=T)
+  y <- rbind(y,x)
+}
+y$subset <- "All"
+
+o <- order(outcomes_order)
+y <- y[order(o),]
+
+write_xlsx(y,"results/epict_analysis_15_linERR_new_outcomesv2_Median.xlsx")
+
