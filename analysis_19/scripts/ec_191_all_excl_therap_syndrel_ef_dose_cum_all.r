532c2ace12dba53afa970773325cf9ae0c1733ca
radiationISGlobal <138576333+radiationISGlobal@users.noreply.github.com>
2023-07-13 12:59:26 +0200

    Add files via upload

---
+++ analysis_19/scripts/ec_191_all_excl_therap_syndrel_ef_dose_cum_all.r
@@ -0,0 +1,40 @@
+# Author:        Francesc Badia
+# Date:         2018-01-17 15:06:37
+# ------------------------------
+# Description:  epict analysis: linERR to all cohort all_excl_therap_syndrel
+# Excluding people with 5% highest cum dose
+# ------------------------------
+
+rm(list=ls())
+setwd('Y:/EPI CT Analysis/2022_new_grouping/')
+source('Y:/EPI CT Analysis/2022_new_grouping/rERR_ModifiedFunctions/rERR_jf.r')
+
+# function to save fit results
+source("analysis_19/scripts/f_results_fit_1.r")
+ 
+dt1 <- readRDS("transformed_cohorts/dt1_new_outcomes_v2.rds")
+lag <- 2
+
+# exclude those subjects with cum exposure > 95%
+dt2 <- dt1[dt1$n_pe==0, c("dose_cum")]
+pct95 <- quantile(dt2$dose_cum, c(0.95))
+dt2 <- dt1[dt1$n_pe==0 & dt1$dose_cum<pct95,]
+
+dt1 <- dt1[dt1$patientids %in% dt2$patientids,]
+rm(dt2)
+
+output_path     <- "analysis_19/results/"
+output_filename <- "ec_res_all_excl_therap_syndrel_ef_dose_cum_all.csv"
+outcome_name    <- "all_excl_therap_syndrel"
+data_set_format <- "event_fromat"
+
+# set the formula for the model
+formula <- Surv(entry_age,exit_age,all_excl_therap_syndrel)~lin(dose_cum)+strata(sex,country,birthcohort)
+
+# fit the model
+fit <- f_fit_linERR_all(formula,dt1,id_name="patientids",time_name="age",lag=lag)
+
+# save results
+res <- f_results_fit_1(fit)
+
+write.table(t(as.data.frame(res)),paste0(output_path,output_filename),row.names = F)
