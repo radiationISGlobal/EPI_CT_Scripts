ccda39f6b1fb1207dde11f87f42b747a8200436c
radiationISGlobal <138576333+radiationISGlobal@users.noreply.github.com>
2023-07-13 12:48:52 +0200

    Add files via upload

---
+++ analysis_10/scripts/ec_106_Myeloid_ef_dose_cum_all.r
@@ -0,0 +1,32 @@
+# Author:        Francesc Badia
+# Date:         2018-01-17 15:06:37
+# ------------------------------
+# Description:  epict analysis: linERR to all cohort Myeloid 5 YEARS LAG
+# ------------------------------
+
+rm(list=ls())
+setwd("Y:/EPI CT Analysis/2022_new_grouping")
+source('Y:/EPI CT Analysis/2022_new_grouping/rERR_ModifiedFunctions/rERR_jf.r')
+
+# function to save fit results
+source("analysis_10/scripts/f_results_fit_1.r")
+
+dt1 <- readRDS("transformed_cohorts/dt1_new_outcomes_v2_lag5.rds")
+lag <- 5
+
+output_path     <- "analysis_10/results/"
+output_filename <- "ec_res_Myeloid_ef_dose_cum_all.csv"
+outcome_name    <- "Myeloid"
+data_set_format <- "event_fromat"
+
+
+# set the formula for the model
+formula <- Surv(entry_age,exit_age,Myeloid)~lin(dose_cum)+strata(sex,country,birthcohort)
+
+# fit the model
+fit <- f_fit_linERR_all(formula,dt1,id_name="patientids",time_name="age",lag=lag)
+
+# save results
+res <- f_results_fit_1(fit)
+
+write.table(t(as.data.frame(res)),paste0(output_path,output_filename),row.names = F)
---
+++ analysis_10/scripts/gather_results.r
@@ -0,0 +1,26 @@
+# Author:        Francesc Badia
+# Date:         2018-01-18 12:39:25
+# ------------------------------
+# Description:  
+# ------------------------------
+
+rm(list=ls())
+setwd("Y:/EPI CT Analysis/2022_new_grouping")
+
+outcomes_order <- c("all_excl_therap_syndrel", "Lymphoid","Myeloid")
+
+files <- list.files("analysis_10/results/",full.names = T)
+y <- data.frame()
+
+for(i in 1:length(files))
+{
+  x <- read.table(files[i],header=T)
+  y <- rbind(y,x)
+}
+y$subset <- "All"
+
+o <- order(outcomes_order)
+y <- y[order(o),]
+
+write_xlsx(y,"results/epict_analysis_10_linERR_lag5_overall_countries.xlsx")
+
