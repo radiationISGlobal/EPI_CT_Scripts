803749a09f1e9f5613ae064cb04e2d36a824c583
radiationISGlobal <138576333+radiationISGlobal@users.noreply.github.com>
2023-07-13 12:54:10 +0200

    Add files via upload

---
+++ analysis_17/scripts/gather_results.r
@@ -0,0 +1,26 @@
+# Author:        Francesc Badia
+# Date:         2018-01-18 12:39:25
+# ------------------------------
+# Description:  
+# ------------------------------
+
+rm(list=ls())
+setwd('Y:/EPI CT Analysis/2022_new_grouping/')
+
+outcomes_order <- c("all_excl_therap_syndrel", "Lymphoid", "Myeloid")
+
+files <- list.files("analysis_17/results/",full.names = T)
+y <- data.frame()
+
+for(i in 1:length(files))
+{
+  x <- read.table(files[i],header=T)
+  y <- rbind(y,x)
+}
+y$subset <- "Excl cumdose > 99%"
+
+o <- order(outcomes_order)
+y <- y[order(o),]
+
+write_xlsx(y,"results/epict_analysis_17_linERR_overall_pct99_countries.xlsx")
+
